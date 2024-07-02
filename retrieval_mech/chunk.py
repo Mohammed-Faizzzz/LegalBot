52b9c9b598d766ad127da17334a456286c50acfb
MustafaAH10 <mustafahussain793@gmail.com>
2024-07-02 13:09:03 +0800

    Add overlap and faiss quantization

--- retrieval_mech/chunk.py
+++ retrieval_mech/chunk.py
@@ -8,14 +8,27 @@ import pickle
 pdf_path = './../pdfs/[2024] SGHC 133.pdf'
 text = extract_text_from_pdf(pdf_path)
 
-def split_into_chunks(text, max_length=512):
+# def split_into_chunks(text, max_length=512):
+#     sentences = sent_tokenize(text)
+#     chunks = []
+#     chunk = ""
+#     for sentence in sentences:
+#         if len(chunk) + len(sentence) > max_length:
+#             chunks.append(chunk)
+#             chunk = sentence
+#         else:
+#             chunk += " " + sentence
+#     chunks.append(chunk)
+#     return chunks
+
+def split_into_chunks(text, max_length=512, overlap = 50):
     sentences = sent_tokenize(text)
     chunks = []
     chunk = ""
     for sentence in sentences:
         if len(chunk) + len(sentence) > max_length:
-            chunks.append(chunk)
-            chunk = sentence
+            chunks.append(chunk.strip())
+            chunk = " ".join(chunk.split()[-overlap:]) + " " + sentence
         else:
             chunk += " " + sentence
     chunks.append(chunk)
