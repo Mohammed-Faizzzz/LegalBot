1d03e204d6d38de7ecca4216ba3d18b9d8039b7b
MustafaAH10 <mustafahussain793@gmail.com>
2024-07-02 13:09:03 +0800

    Add overlap and faiss quantization

--- index_embeddings.py
+++ index_embeddings.py
@@ -4,7 +4,9 @@ import numpy as np
 
 embeddings = np.load('embeddings.npy')
 d = embeddings.shape[1]  # Dimensions
-index = faiss.IndexFlatL2(d)  # Use L2 distance
+quantizer = faiss.IndexFlatL2(d)
+index = faiss.IndexFlatL2(quantizer, d, nlist = 100)  # Use L2 distance
+index.train(embeddings)
 index.add(embeddings)
 
 # Save
