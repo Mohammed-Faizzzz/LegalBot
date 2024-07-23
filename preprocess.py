93fb6c90721ac4ec27297bfffcab8bd45991cab1
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2024-07-23 21:45:27 +0800

    Fix error handling in Inference Class

--- preprocess.py
+++ preprocess.py
@@ -2,19 +2,10 @@ import pandas as pd
 from datasets import Dataset
 from transformers import AutoTokenizer
 
-# Load your data
 df = pd.read_csv('dataset.csv')
-
-# select only the first 200 rows for demonstration
 df = df.head(200)
-
-# Convert to Hugging Face Dataset
 dataset = Dataset.from_pandas(df)
-
-# Split the dataset
 dataset = dataset.train_test_split(test_size=0.2)
-
-# Save the dataset
 dataset.save_to_disk("dataset")
 
 # Tokenize the dataset
@@ -44,12 +35,10 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-# Apply the preprocessing function
 tokenized_dataset = dataset.map(preprocess_function, batched=True)
 
 # Remove the original columns
 columns_to_remove = dataset['train'].column_names
 tokenized_dataset = tokenized_dataset.remove_columns(columns_to_remove)
 
-# Save the tokenized dataset
 tokenized_dataset.save_to_disk("tokenized_dataset_t5")
