2fe9489b911c910ff1c48d5ece756b6a1d82e087
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2024-07-30 12:14:38 +0800

    Add styling to Hero Section

--- client/src/pages/Home/sections/HeroSection.js
+++ client/src/pages/Home/sections/HeroSection.js
@@ -6,12 +6,12 @@ const HeroSection = () => {
     <section id="hero">
       <div className="hero-main">
         <div className="hero-content">
-          <h1>Accelerate Legal Research, Win Cases Faster</h1>
-          <p>Harness AI to find relevant legal precedents in seconds, not hours</p>
+          <h1>LexCelerate.</h1>
+          <p>Accelerate Legal Research, Win Cases Faster</p>
           <button>Try Now</button>
         </div>
       </div>
-      <div className="hero-side">
+      {/* <div className="hero-side">
         <div className="user-guide">
           <h2>How It Works</h2>
           <ol>
@@ -26,12 +26,10 @@ const HeroSection = () => {
           <h2 style={{textAlign: "center"}}>Hear From Our Clients</h2>
           <p>"BuildKwik has transformed our business! The user-friendly interface and seamless integration with our existing systems have made our operations much more efficient."</p>
           <h4>- Alex P.</h4>
-          {/* <p>"A game-changer in the industry. The AI-powered features have significantly improved our customer interactions and overall satisfaction."</p>
-          <h4>- Jamie L.</h4> */}
           <p>"Exceptional service and support. The BuildKwik team has been incredibly responsive and helpful in addressing our needs and questions."</p>
           <h4>- Morgan R.</h4>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
